f4507df537c82a2576dea889d7aec3a72b9b5cdc
AlanF23 <alanf200023unam@gmail.com>
2024-05-23 16:54:03 -0300

    arreglos para que ande

--- telegrambot/telegrambot.py
+++ telegrambot/telegrambot.py
@@ -27,7 +27,7 @@ async def acercade(update: Update, context):
 async def kill(update: Update, context):
     logging.info(context.args)
     if context.args and context.args[0] == '@e':
-        await context.bot.send_animation(update.message.chat.id, "CgACAgEAAxkBAAOPZkuctzsWZVlDSNoP9PavSZmH5poAAmUCAALrx0lEVKaX7K-68Ns1BA")
+        await context.bot.send_animation(update.message.chat.id, "CgACAgQAAxkBAAMJZk-RMAb2CuP8LWR5bnrFExFGpgwAAh8DAALJ4axTw0mii4VDh981BA")
         await asyncio.sleep(6)
         await context.bot.send_message(update.message.chat.id, text="¡¡¡Ahora estan todos muertos!!!")
     else:
