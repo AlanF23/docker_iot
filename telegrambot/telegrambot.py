12392b6624a0ca70bcdb59a63a72844c406ab2b8
AlanF23 <alanf200023unam@gmail.com>
2024-05-30 16:51:31 -0300

    funcionando

--- telegrambot/telegrambot.py
+++ telegrambot/telegrambot.py
@@ -1,8 +1,9 @@
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
-import logging, os, asyncio, aiomysql, traceback, locale
+import logging, os, asyncio, aiomysql, traceback, locale, ssl, certifi, json
 import matplotlib.pyplot as plt
 from io import BytesIO
+import aiomqtt
 
 token=os.environ["TB_TOKEN"]
 
@@ -22,8 +23,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(update.message.chat.id, text="Bienvenido al Bot "+ nombre + " " + apellido,reply_markup=ReplyKeyboardMarkup(kb))
 
 async def acercade(update: Update, context):
-    await context.bot.send_message(update.message.chat.id, text="Este bot fue creado para el curso de IoT FIO")
-
+    await context.bot.send_message(update.message.chat.id, text="Este bot fue creado por AlanF23 para el curso de IoT FIO")
+'''
 async def kill(update: Update, context):
     logging.info(context.args)
     if context.args and context.args[0] == '@e':
@@ -31,7 +32,7 @@ async def kill(update: Update, context):
         await asyncio.sleep(6)
         await context.bot.send_message(update.message.chat.id, text="¡¡¡Ahora estan todos muertos!!!")
     else:
-        await context.bot.send_message(update.message.chat.id, text="☠️ ¡¡¡Esto es muy peligroso!!! ☠️")
+        await context.bot.send_message(update.message.chat.id, text="☠️ ¡¡¡Esto es muy peligroso!!! ☠️")'''
         
 async def medicion(update: Update, context):
     logging.info(update.message.text)
@@ -107,36 +108,128 @@ async def graficos(update: Update, context):
         if conn:
             conn.close()
 
+#ACA EMPIEZA LO MIO
+
 async def setpoint(update: Update, context):
-    logging.info(context.args)
-    if context.args and context.args[0] == '25':  #Aca tengo que hacer que solo sea numeros
-        await context.bot.send_message(update.message.chat.id, text="Valor de temperatura seteado en {}".format(context.args[0]))
-    else:
-        await context.bot.send_message(update.message.chat.id, text="Ingrese un valor correcto de seteo")
+    logging.info(update.message.text)
+    tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    tls_context.verify_mode = ssl.CERT_REQUIRED
+    tls_context.check_hostname = True
+    tls_context.load_default_certs()
+    async with aiomqtt.Client(
+        os.environ["SERVIDOR"],
+        username=os.environ["MQTT_USR"],
+        password=os.environ["MQTT_PASS"],
+        port=int(os.environ["PUERTO_MQTTS"]),
+        tls_context=tls_context,
+    ) as client:
+        topico = update.message.text.split()[0]
+        topico=topico[1:]
+        try:
+            if float(context.args[0]) > 0.0 and float(context.args[0]) < 60.0:
+                await client.publish(topic=topico, payload=context.args[0] , qos=1)
+                await context.bot.send_message(update.message.chat.id, text="Valor de temperatura seteado en {}".format(context.args[0]))
+            else:
+                await context.bot.send_message(update.message.chat.id, text="Ingrese un valor correcto de seteo")
+        except ValueError:
+            await context.bot.send_message(update.message.chat.id, text="Ingrese un valor correcto de seteo")
 
 async def modo(update: Update, context):
     logging.info(context.args)
-    if context.args and (context.args[0] == 'auto' or context.args[0] == 'manual'):
-        await context.bot.send_message(update.message.chat.id, text="Modo actual: {}".format(context.args[0]))
-    else:
-        await context.bot.send_message(update.message.chat.id, text="Ingrese un modo válido")
+    tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    tls_context.verify_mode = ssl.CERT_REQUIRED
+    tls_context.check_hostname = True
+    tls_context.load_default_certs()
+    async with aiomqtt.Client(
+        os.environ["SERVIDOR"],
+        username=os.environ["MQTT_USR"],
+        password=os.environ["MQTT_PASS"],
+        port=int(os.environ["PUERTO_MQTTS"]),
+        tls_context=tls_context,
+    ) as client:
+        topico = update.message.text.split()[0]
+        topico=topico[1:]
+        try:
+            if context.args and (context.args[0] == 'auto' or context.args[0] == 'manual'):
+                await client.publish(topic=topico, payload=context.args[0] , qos=1)
+                await context.bot.send_message(update.message.chat.id, text="Modo actual: {}".format(context.args[0]))
+            else:
+                await context.bot.send_message(update.message.chat.id, text="Ingrese un modo válido")
+        except ValueError:
+            await context.bot.send_message(update.message.chat.id, text="Ingrese un modo valido")
 
 async def periodo(update: Update, context):
     logging.info(context.args)
-    if context.args and context.args[0] == '10':  #Aca tengo que hacer que solo sea numeros también
-        await context.bot.send_message(update.message.chat.id, text="Valor de periodo seteado en {}".format(context.args[0]))
-    else:
-        await context.bot.send_message(update.message.chat.id, text="Ingrese un valor correcto de periodo")
+    tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    tls_context.verify_mode = ssl.CERT_REQUIRED
+    tls_context.check_hostname = True
+    tls_context.load_default_certs()
+    async with aiomqtt.Client(
+        os.environ["SERVIDOR"],
+        username=os.environ["MQTT_USR"],
+        password=os.environ["MQTT_PASS"],
+        port=int(os.environ["PUERTO_MQTTS"]),
+        tls_context=tls_context,
+    ) as client:
+        topico = update.message.text.split()[0]
+        topico=topico[1:]
+        try:
+            if float(context.args[0]) > 0.0 and float(context.args[0]) < 60.0:
+                await client.publish(topic=topico, payload=context.args[0] , qos=1)
+                await context.bot.send_message(update.message.chat.id, text="Periodo seteado en {} segundos".format(context.args[0]))
+            else:
+                await context.bot.send_message(update.message.chat.id, text="Ingrese un valor correcto de periodo")
+        except ValueError:
+            await context.bot.send_message(update.message.chat.id, text="Ingrese un valor correcto de periodo")
 
 async def destello(update: Update, context):
     logging.info(update.message.text)
-    await context.bot.send_message(update.message.chat.id, text="Destellandoo")
+    tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    tls_context.verify_mode = ssl.CERT_REQUIRED
+    tls_context.check_hostname = True
+    tls_context.load_default_certs()
+    async with aiomqtt.Client(
+        os.environ["SERVIDOR"],
+        username=os.environ["MQTT_USR"],
+        password=os.environ["MQTT_PASS"],
+        port=int(os.environ["PUERTO_MQTTS"]),
+        tls_context=tls_context,
+    ) as client:
+        topico = update.message.text
+        try:
+            await client.publish(topic=topico, payload=1, qos=1)
+            await context.bot.send_message(update.message.chat.id, text="Destellando")
+        except ValueError:
+            await context.bot.send_message(update.message.chat.id, text="No se pudo destellar")
+
+async def rele(update: Update, context):
+    logging.info(context.args)
+    tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    tls_context.verify_mode = ssl.CERT_REQUIRED
+    tls_context.check_hostname = True
+    tls_context.load_default_certs()
+    async with aiomqtt.Client(
+        os.environ["SERVIDOR"],
+        username=os.environ["MQTT_USR"],
+        password=os.environ["MQTT_PASS"],
+        port=int(os.environ["PUERTO_MQTTS"]),
+        tls_context=tls_context,
+    ) as client:
+        topico = update.message.text.split()[0]
+        topico=topico[1:]
+        try:
+            if context.args and (context.args[0] == 'encendido' or context.args[0] == 'apagado'):
+                await client.publish(topic=topico, payload=context.args[0] , qos=1)
+                await context.bot.send_message(update.message.chat.id, text="Estado relé: {}".format(context.args[0]))
+            else:
+                await context.bot.send_message(update.message.chat.id, text="Ingrese un estado de relé válido")
+        except ValueError:
+            await context.bot.send_message(update.message.chat.id, text="Ingrese un estado de relé valido")
 
 def main():
     application = Application.builder().token(token).build()
     application.add_handler(CommandHandler('start', start))
     application.add_handler(CommandHandler('acercade', acercade))
-    application.add_handler(CommandHandler('kill', kill))
     application.add_handler(MessageHandler(filters.Regex("^(temperatura|humedad)$"), medicion))
     application.add_handler(MessageHandler(filters.Regex("^(gráfico temperatura|gráfico humedad)$"), graficos))
 
@@ -144,6 +237,7 @@ def main():
     application.add_handler(CommandHandler('setpoint', setpoint))
     application.add_handler(CommandHandler('modo', modo))
     application.add_handler(CommandHandler('periodo', periodo))
+    application.add_handler(CommandHandler('rele', rele))
     application.add_handler(MessageHandler(filters.Regex("^(destello)$"), destello))
 
     application.run_polling()
