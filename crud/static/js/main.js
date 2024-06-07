790521936bcda0fa86fec9710aaaf13819a4f249
AlanF23 <alanf200023unam@gmail.com>
2024-06-07 10:56:38 -0300

    algunas pruebas que no funcionan, verificar head y body correctos

--- crud/static/js/main.js
+++ crud/static/js/main.js
@@ -10,3 +10,17 @@ if(btnDelete) {
     });
   })
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  const themeSelector = document.getElementById('themeSelector');
+  const currentTheme = localStorage.getItem('theme') || 'dark';
+  
+  document.getElementById('themeStylesheet').setAttribute('href', `https://bootswatch.com/5/${currentTheme}/bootstrap.min.css`);
+  themeSelector.value = currentTheme;
+
+  themeSelector.addEventListener('change', function () {
+    const selectedTheme = themeSelector.value;
+    localStorage.setItem('theme', selectedTheme);
+    document.getElementById('themeStylesheet').setAttribute('href', `https://bootswatch.com/5/${selectedTheme}/bootstrap.min.css`);
+  });
+});
