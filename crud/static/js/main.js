fb3fe3acf8a00b6cb9e3f7ca062fb8114215d478
AlanF23 <alanf200023unam@gmail.com>
2024-06-11 15:38:23 -0300

    terminado

--- crud/static/js/main.js
+++ crud/static/js/main.js
@@ -10,17 +10,3 @@ if(btnDelete) {
     });
   })
 }
-
-document.addEventListener('DOMContentLoaded', function () {
-  const themeSelector = document.getElementById('themeSelector');
-  const currentTheme = localStorage.getItem('theme') || 'dark';
-  
-  document.getElementById('themeStylesheet').setAttribute('href', `https://bootswatch.com/5/${currentTheme}/bootstrap.min.css`);
-  themeSelector.value = currentTheme;
-
-  themeSelector.addEventListener('change', function () {
-    const selectedTheme = themeSelector.value;
-    localStorage.setItem('theme', selectedTheme);
-    document.getElementById('themeStylesheet').setAttribute('href', `https://bootswatch.com/5/${selectedTheme}/bootstrap.min.css`);
-  });
-});
