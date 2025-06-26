619d001bf158da4eead1f7be105dd4558d45cad7
Alan Fucks <alanf200023unam@gmail.com>
2025-06-26 16:35:13 -0300

    para que ande el mqtt

--- clienteMqtt/clienteMqtt.py
+++ clienteMqtt/clienteMqtt.py
@@ -8,15 +8,15 @@ env.read_env() #lee el archivo con las variables. por defecto .env
 logging.basicConfig(format='%(asctime)s - cliente mqtt - %(levelname)s:%(message)s', level=logging.INFO, datefmt='%d/%m/%Y %H:%M:%S')
 
 async def main():
-    tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-    tls_context.verify_mode = ssl.CERT_REQUIRED
-    tls_context.check_hostname = True
-    tls_context.load_default_certs()
+    #tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    #tls_context.verify_mode = ssl.CERT_REQUIRED
+    #tls_context.check_hostname = True
+    #tls_context.load_default_certs()
 
     async with aiomqtt.Client(
         env("SERVIDOR"),
-        port=8883,
-        tls_context=tls_context,
+        #port=8883,
+        #tls_context=tls_context,
     ) as client:
         await client.subscribe("#")
         async for message in client.messages:
