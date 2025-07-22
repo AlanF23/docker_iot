ee9c5414577285255c7b4af402c6230915b6a3c0
Alan Fucks <alanf200023unam@gmail.com>
2025-07-22 18:05:53 -0300

    funciona el mqtt con mosquitto

--- clienteMqtt/clienteMqtt.py
+++ clienteMqtt/clienteMqtt.py
@@ -1,10 +1,9 @@
-import asyncio, logging, os, aiomysql, json, traceback
+import asyncio, ssl, certifi, logging, os, aiomysql, json, traceback
 import aiomqtt
 
 logging.basicConfig(format='%(asctime)s - cliente mqtt - %(levelname)s:%(message)s', level=logging.INFO, datefmt='%d/%m/%Y %H:%M:%S %z')
 
 async def main():
-
     async with aiomqtt.Client(
         os.environ['SERVIDOR'],
         port=1883,
@@ -12,22 +11,26 @@ async def main():
         await client.subscribe(os.environ['TOPICO'])
         async for message in client.messages:
             logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
-            dispositivo=str(message.topic).split('/')[-1]
             datos=json.loads(message.payload.decode('utf8'))
-            sql = "INSERT INTO `mediciones` (`sensor_id`, `temperatura`, `humedad`) VALUES (%s, %s, %s)"
+            dispositivo=str(message.topic).split('/')[1]
+            logging.info("Datos parseados: %s", datos)
+            logging.info("Dispositivo: %s", dispositivo)
+            sql = "INSERT INTO `mediciones` (`sensor_id`, `temperatura`, `turbidez`) VALUES (%s, %s, %s)"
             try:
+                logging.info("Conectando a la base de datos...")
                 conn = await aiomysql.connect(host=os.environ["MARIADB_SERVER"], port=3306,
                                             user=os.environ["MARIADB_USER"],
                                             password=os.environ["MARIADB_USER_PASS"],
                                             db=os.environ["MARIADB_DB"])
+                logging.info("Conectado a la base de datos.")
             except Exception as e:
-                logging.error(traceback.format_exc())
+                logging.error(traceback.format_exc()) 
 
             cur = await conn.cursor()
 
             async with conn.cursor() as cur:
                 try:
-                    await cur.execute(sql, (dispositivo, datos['temperatura'], datos['humedad']))
+                    await cur.execute(sql, (dispositivo, datos['temperatura'], datos['turbidez']))
                     await conn.commit()
                     await cur.close()
                     await conn.ensure_closed()
